91439c4f6677bbbcad470ee444793479c7c38cf0
Erik Sales <eriksales1@hotmail.com>
2022-11-29 11:11:57 -0500

    Header and sidebar updates, not finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Header } from "./components/Header"
 import  { Post }  from "./Post"
 
+import styles from './App.module.css';
 
 import './global.css';
 
@@ -12,16 +13,21 @@ export function App() {
   return (
     <div>
       <Header />
-        <Post  
-          author="Erik Sales" 
-          content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores, iure temporibus ipsa, illo ducimus debitis aspernatur dicta, quo velit est nulla? Mollitia expedita illo esse aliquam iusto rerum voluptate illum!"
-        />
-        <Post 
-        author="Karol Lustosa"
-        content="Novo poste gerado teste"
-        />
-       
-   
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Erik Sales"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores, iure temporibus ipsa, illo ducimus debitis aspernatur dicta, quo velit est nulla? Mollitia expedita illo esse aliquam iusto rerum voluptate illum!"
+          />
+          <Post
+            author="Karol Lustosa"
+            content="Novo poste gerado teste"
+          />
+        </main>
+
+      </div>
     </div>
   )
 }
