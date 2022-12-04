2ebcca9d1f5ec26525b5b3a8521b5bf1d473801e
Erik Sales <eriksales1@hotmail.com>
2022-12-03 20:10:35 -0500

    Comment component update automation

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -4,7 +4,7 @@ import styles from './Comment.module.css'
 
 
 
-export function Comment(){
+export function Comment({content}){
     return(
      <div className={styles.comment}>
            <Avatar hasBorder={false}src="https://github.com/erikpolsci.png"/> 
@@ -23,7 +23,7 @@ export function Comment(){
                 </button>
                 </header>
                 <p>
-                    Thats nice!
+                    {content}
                 </p>
             </div>
 
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,35 +1,77 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import { useState } from 'react';
+
+
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
+
 import styles from './Post.module.css'
 
 
 
-export function Post(props) {
+
+export function Post({author, publishedAt, content}) {
+    const [comments, setComments] = useState([
+       'Post muito bacana'
+    ])
+
+    const [newCommentText, setNewCommentText] = useState('')
+
+    const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'as' HH:mm'h'");
+
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        addSuffix:true
+    } )
+
+    function handleCreateNewComment() {
+        event.preventDefault()
+
+        setComments([...comments, newCommentText]);
+        setNewCommentText('');
+
+
+    }
+
+    function handleNewCommentChange() {
+        setNewCommentText(event.target.value);
+    }
+
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src={props.author.avatarUrl} 
+                    <Avatar src={author.avatarUrl} 
                     />
                     <div className={styles.authorInfo}>
-                        <strong>{props.author.name}</strong>
-                        <span>{props.author.role}</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
               </div>
 
-              <time title="november 30 12:07pm"dateTime="2022-11-30 12:06:30">Publicado ha 1h</time>
+              <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+                { publishedDateRelativeToNow }              
+              </time>
             </header>
 
             <div className={styles.content}>
-                
+                {content.map(line => {
+                   if(line.type === 'paragraph'){
+                    return <p>{line.content}</p>;
+                   } else if(line.type === 'link') {
+                    return <p><a href="#">{line.content}</a></p>;
+
+                   }
+                })}
                 
             </div>
 
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
 
                 <textarea 
                 placeholder='Deixe um comentario'
+                value={newCommentText}
+                onChange={handleNewCommentChange}
                 />
                 <footer>
                     <button type="submit">Publicar</button>
@@ -37,9 +79,9 @@ export function Post(props) {
             </form>
 
             <div className={styles.commentList}>
-                <Comment />
-                <Comment />
-                <Comment />
+             {comments.map(comment => {
+                return <Comment content={comment}/>
+             })}
 
             </div>
 
